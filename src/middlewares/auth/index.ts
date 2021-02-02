c162e1a26c9ff956d62fb5328e244e2ae9f36e7a
Felipe Megale <felipe.megale17@gmail.com>
2021-02-02 22:34:14 +0000

    implement auth middleware

---
+++ src/middlewares/auth/index.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as jwt from "jsonwebtoken";
+import { getConnection } from "typeorm";
+import JWTPayload from "../../interfaces/JWTPayload";
+import User from "../../entity/User";
+
+const router = async (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+    const token = authorization.split(" ")[1];
+
+    if (!token) {
+        throw new Error("NoTokenException");
+    }
+
+    try {
+        const now = new Date().getTime();
+        const decodedToken = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        ) as JWTPayload;
+
+        if (decodedToken.exp <= now) {
+            throw new Error("TokenExpiredException");
+        }
+
+        const user = await getConnection().getRepository(User).findOne({
+            email: decodedToken.email,
+            username: decodedToken.username,
+        });
+
+        if (!user) {
+            throw new Error("UnauthorizedAccessException");
+        }
+
+        next();
+    } catch (err) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            data: "",
+            error: err.message,
+            statusCode: StatusCodes.UNAUTHORIZED,
+        });
+    }
+};
+
+export default router;
