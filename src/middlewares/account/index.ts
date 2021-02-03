f1638411e0c8070e8ee42c72515bad230650cfad
Felipe Megale <felipe.megale17@gmail.com>
2021-02-03 21:49:19 +0000

    format file

--- src/middlewares/account/index.ts
+++ src/middlewares/account/index.ts
@@ -7,10 +7,6 @@ const wrapper = (): Router => {
     const router = Router();
     const _accountService = new AccountService();
 
-    router.get("/", (req, res) => {
-        res.send("oie from account middleware");
-    });
-
     router.post("/signup", async (req, res) => {
         const signUpData: SignUpDataDTO = req.body;
         const ret = await _accountService.signUp(signUpData);
@@ -18,13 +14,13 @@ const wrapper = (): Router => {
         res.status(ret.statusCode).json(ret);
     });
 
-    router.post("/signin", async (req,res) => {
+    router.post("/signin", async (req, res) => {
         const signInData: SignInDataDTO = req.body;
         const loggedIn = await _accountService.signIn(signInData);
         res.status(loggedIn.statusCode).json(loggedIn);
     });
 
-    router
+    router;
 
     return router;
 };
