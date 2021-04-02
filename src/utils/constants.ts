f446f2df5f6a43b628490de6a4e004fdf931a97b
Felipe Megale <felipe.megale17@gmail.com>
2021-04-02 14:43:08 -0300

    put multer max file size in constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
 export const MORGAN_FORMAT =
-    "HTTP/:http-version - :remote-addr - :method :url - :status - Response :response-time ms - Total :total-time ms - :user-agent";
+    'HTTP/:http-version - :remote-addr - :method :url - :status - Response :response-time ms - Total :total-time ms - :user-agent';
 export const BCRYPT_ROUNDS = 10;
+export const MAX_AVATAR_FILE_SIZE = 10000000; // 10MB
