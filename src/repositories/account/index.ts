5d1bbce848138981299a75e25093cdbfb168456a
Felipe Megale <felipe.megale17@gmail.com>
2021-04-02 14:43:41 -0300

    remove unused repository

--- src/repositories/account/index.ts
+++
@@ -1,8 +0,0 @@
-import SignUpDataDTO from "../../interfaces/SignUpDataDTO";
-
-export default class AccountRepository {
-    async signUp(signUpData: SignUpDataDTO) {
-        console.log(signUpData);
-        return signUpData;
-    }
-}
